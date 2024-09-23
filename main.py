3d2ada84fe36de9b50a1a39b5c037da646697226
Mauricio H Nagaoka <mhnagaoka@gmail.com>
2024-09-22 21:24:28 -0300

    Moves pieces according to the player's input

--- main.py
+++ main.py
@@ -30,15 +30,15 @@ screen = pygame.display.set_mode((640, 480))
 clock = pygame.time.Clock()
 running = True
 board = Board()
-moves = [
-    Shape.rotate_clockwise,
-    Shape.rotate_counterclockwise,
-    Shape.move_left,
-    Shape.move_right,
-    Shape.move_down,
-    None
-]
 shape = Shape.create_random_shape().move(4, 0)
+
+keys_monitored = [pygame.K_LEFT, pygame.K_RIGHT, pygame.K_a, pygame.K_d, pygame.K_e, pygame.K_q]
+prev_keys = pygame.key.get_pressed()
+
+last_right = 0
+last_left = 0
+last_down = 0
+last_fall = pygame.time.get_ticks()
 
 while running:
     # poll for events
@@ -47,12 +47,54 @@ while running:
         if event.type == pygame.QUIT:
             running = False
 
-    new_shape = None
-    random_move = random.choice(moves)
-    if random_move:
-        new_shape = random_move(shape)
-        if not board.is_colliding(new_shape):
-            shape = new_shape
+
+    # Detects new key presses
+    curr_keys = pygame.key.get_pressed()
+    new_keys = [key for key in keys_monitored if curr_keys[key] and not prev_keys[key]]
+    prev_keys = curr_keys
+
+    # Add moves to the list of moves to be executed according to the keys pressed
+    moves = []
+    current_time = pygame.time.get_ticks()
+    
+    if curr_keys[pygame.K_LEFT] or curr_keys[pygame.K_a]:
+        if current_time - last_left > 100:
+            moves.append(Shape.move_left)
+            last_left = current_time
+    if curr_keys[pygame.K_RIGHT] or curr_keys[pygame.K_d]:
+        if current_time - last_right > 100:
+            moves.append(Shape.move_right)
+            last_right = current_time
+    if curr_keys[pygame.K_DOWN] or curr_keys[pygame.K_s]:
+        if current_time - last_right > 100:
+            moves.append(Shape.move_down)
+            last_down = current_time
+    # if pygame.K_LEFT in new_keys or pygame.K_a in new_keys:
+    #     moves.append(Shape.move_left)
+    # if pygame.K_RIGHT in new_keys or pygame.K_d in new_keys:
+    #     moves.append(Shape.move_right)
+    if pygame.K_e in new_keys:
+        moves.append(Shape.rotate_clockwise)
+    if pygame.K_q in new_keys:
+        moves.append(Shape.rotate_counterclockwise)
+    
+    # Tries to move the shape according to the list of moves
+    new_shape = shape
+    for move in moves:
+        new_shape = move(new_shape)
+    if not board.is_colliding(new_shape):
+        shape = new_shape        
+
+    if current_time - last_fall > 1000:
+        if board.can_move(shape, 0, 1):
+            shape = shape.move_down()
+        else:
+            board.fuse(shape)
+            shape = Shape.create_random_shape().move(4, 0)
+            if board.is_topped_out(shape):
+                # Game over
+                running = False
+        last_fall = current_time
 
     # fill the screen with a color to wipe away anything from last frame
     screen.fill("gray33")
@@ -63,19 +105,16 @@ while running:
     # flip() the display to put your work on screen
     pygame.display.flip()
 
-    clock.tick(5)  # limits FPS to 60
+    clock.tick(60)  # limits FPS to 60
 
-    moved_shape = shape.move(0, 1)
-    if not board.is_colliding(moved_shape):
-        shape = moved_shape
-    else:
-        board.fuse(shape)
-        shape = Shape.create_random_shape().move(4, 0)
-        if board.is_topped_out(shape):
-            # Game over
-            running = False
-        else:
-            log.debug("New shape")
+    # if board.is_colliding(shape):
+    #     board.fuse(shape)
+    #     shape = Shape.create_random_shape().move(4, 0)
+    #     if board.is_topped_out(shape):
+    #         # Game over
+    #         running = False
+    #     else:
+    #         log.debug("New shape")
 print("Game over")
 clock.tick(1)
 pygame.quit()
